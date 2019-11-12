dfe42d833f4fee4d7ae1de82f0201ef797c19c6f
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-11-12 08:19:23 -0600

    made user interface class

--- Sweepstakes/Contestant.cs
+++ Sweepstakes/Contestant.cs
@@ -8,5 +8,27 @@ namespace Sweepstakes
 {
     class Contestant
     {
+        //member variables
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNum;
+
+        //member methods
+        public void AssignFirstName()
+        {
+            Console.WriteLine("Please enter your first name.");
+            firstName = Console.ReadLine();
+        }
+        public void AssignLastName()
+        {
+            Console.WriteLine("Please enter your last name.");
+            lastName = Console.ReadLine();
+        }
+        public void CollectEmailAddress()
+        {
+            Console.WriteLine("Please enter your email address.");
+            emailAddress = Console.ReadLine();
+        }
     }
 }
---
+++ Sweepstakes/UserInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public static class UserInterface
+    {
+
+        public string GetName()
+        {
+            
+            Console.WriteLine("Please enter your first name.");
+         
+        }
+    }
+}
